974753f40809e341081912487ad6a7d03502a111
Itxaka <itxaka.garcia@spectrocloud.com>
2023-10-15 17:28:11 +0200

    Fix network module (#488)

--- packages/dracut/kairos-network/files/90kairos-network/module-setup.sh
+++ packages/dracut/kairos-network/files/90kairos-network/module-setup.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 # This module selects the proper network module to be used by dracut
-# while avoiding using systemd-networkd
 # This helps it be more generic and work on all kairos flavors easily without having
 # to specify the network module directly in our framework packages list
 
 # called by dracut
 check() {
-    return 255
+    # always include this module
+    return 0
 }
 
 # called by dracut
 depends() {
-    is_qemu_virtualized && echo -n "qemu-net "
+    # Network module: selects underlying network module (conman, networkmanager, network-legacy, systemd-networkd)
+    # network module needs to be installed for livenet module to work
+    # network-legacy just uses bash scripts to setup the network
+    # ubuntu-20 uses network
+    # ubuntu-22 uses network+network-legacy
+    # ubuntu-latest, debian, fedora,opensuse-*, almalinux, rockylinux uses network+network-legacy+systemd-networkd
+    # Im guessing we need the network module in the initramfs in case we set the rd.neednet=1
+    # but that only affects conman, networkmanager and network-legacy?
 
-    for module in network network-legacy; do
-        if dracut_module_included "$module"; then
-            network_handler="$module"
-            break
-        fi
-    done
-
-    if [ -z "$network_handler" ]; then
-        if check_module "network-legacy"; then
-            network_handler="network-legacy"
-        else
-            network_handler="network"
-        fi
+    # shellcheck disable=SC2144
+    # add network-legacy module if it exists
+    if [ -d "${dracutbasedir}"/modules.d/??network-legacy ]; then
+        network_handler="network-legacy"
     fi
-    echo "kernel-network-modules $network_handler"
+
+    echo "kernel-network-modules network $network_handler"
     return 0
 }
 
