6c71b644f28a247640b9b83529b49bb1c29d1658
Itxaka <itxaka@kairos.io>
2024-10-01 10:59:32 +0200

    Use kairos-release instead of os-release
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- packages/system/suc-upgrade/suc-upgrade.sh
+++ packages/system/suc-upgrade/suc-upgrade.sh
@@ -3,15 +3,28 @@ set -x -e
 HOST_DIR="${HOST_DIR:-/host}"
 
 if [ "$FORCE" != "true" ]; then
-    if diff /etc/os-release $HOST_DIR/etc/os-release >/dev/null; then
-        echo Update to date with
-        cat /etc/os-release
-        exit 0
+    if [ -f "/etc/kairos-release" ]; then
+      UPDATE_VERSION=$(source /etc/kairos-release && echo "${KAIROS_VERSION}")
+    else
+      UPDATE_VERSION=$(source /etc/os-release && echo "${KAIROS_VERSION}")
+    fi
+
+    if [ -f "/etc/kairos-release" ]; then
+      CURRENT_VERSION=$(source "${HOST_DIR}"/etc/kairos-release && echo "${KAIROS_VERSION}")
+    else
+      CURRENT_VERSION=$(source "${HOST_DIR}"/etc/os-release && echo "${KAIROS_VERSION}")
+    fi
+
+    if [ "$CURRENT_VERSION" == "$UPDATE_VERSION" ]; then
+      echo Update to date
+      echo "Current version: ${CURRENT_VERSION}"
+      echo "Update version: ${UPDATE_VERSION}"
+      exit 0
     fi
 fi
 
-mount --rbind $HOST_DIR/dev /dev
-mount --rbind $HOST_DIR/run /run
+mount --rbind "${HOST_DIR}"/dev /dev
+mount --rbind "${HOST_DIR}"/run /run
 kairos-agent upgrade --source dir:/
 nsenter -i -m -t 1 -- reboot
 exit 1
