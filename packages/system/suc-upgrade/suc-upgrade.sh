875833fc80f40e5305c6cd0818ac1f5af461c8d8
Dimitris Karakasilis <jimmykarily@gmail.com>
2024-10-02 16:26:32 +0300

    Implement `--recovery` flag in suc-upgrade script (#1087)
    To allow users to upgrade recovery from kubernetes
    
    Part of: https://github.com/kairos-io/kairos/issues/2760
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- packages/system/suc-upgrade/suc-upgrade.sh
+++ packages/system/suc-upgrade/suc-upgrade.sh
@@ -12,6 +12,18 @@ fi
 
 mount --rbind $HOST_DIR/dev /dev
 mount --rbind $HOST_DIR/run /run
-kairos-agent upgrade --source dir:/
+
+recovery_mode=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --recovery) recovery_mode=true;;
+    esac
+    shift
+done
+if [ "$recovery_mode" = true ]; then
+    kairos-agent upgrade --recovery --source dir:/
+else
+    kairos-agent upgrade --source dir:/
+fi
 nsenter -i -m -t 1 -- reboot
 exit 1
