9232f7eed6474a3e2323a0645142a213b1ffe323
Itxaka <itxaka.garcia@spectrocloud.com>
2024-10-07 11:43:48 +0200

    Use kairos-release instead of os-release (#1082)

--- packages/system/suc-upgrade/suc-upgrade.sh
+++ packages/system/suc-upgrade/suc-upgrade.sh
@@ -3,10 +3,23 @@ set -x -e
 HOST_DIR="${HOST_DIR:-/host}"
 
 if [ "$FORCE" != "true" ]; then
-    if diff /etc/os-release $HOST_DIR/etc/os-release >/dev/null; then
-        echo Update to date with
-        cat /etc/os-release
-        exit 0
+    if [ -f "/etc/kairos-release" ]; then
+      UPDATE_VERSION=$(source /etc/kairos-release && echo "${KAIROS_VERSION}")
+    else
+      UPDATE_VERSION=$(source /etc/os-release && echo "${KAIROS_VERSION}")
+    fi
+
+    if [ -f "/etc/kairos-release" ]; then
+      CURRENT_VERSION=$(source "${HOST_DIR}"/etc/kairos-release && echo "${KAIROS_VERSION}")
+    else
+      CURRENT_VERSION=$(source "${HOST_DIR}"/etc/os-release && echo "${KAIROS_VERSION}")
+    fi
+
+    if [ "$CURRENT_VERSION" == "$UPDATE_VERSION" ]; then
+      echo Up to date
+      echo "Current version: ${CURRENT_VERSION}"
+      echo "Update version: ${UPDATE_VERSION}"
+      exit 0
     fi
 fi
 
